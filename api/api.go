b9524f7de4347898ca8aaf55b803eb18651184a1
Hugo Torres <hugo_torres@ultimatesoftware.com>
2018-08-30 11:25:43 -0600

    Add scaffolding for consuming and getting messages

---
+++ api/api.go
@@ -0,0 +1,59 @@
+package api
+
+import (
+	"io/ioutil"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type Server struct {
+	*http.Server
+	msgs chan string
+}
+
+func NewServer(addr string, msgs chan string) error {
+	mux := http.NewServeMux()
+
+	srv := &Server{
+		Server: &http.Server{
+			Addr:    addr,
+			Handler: mux,
+		},
+
+		msgs: msgs,
+	}
+
+	mux.HandleFunc("/message", srv.handleMessages)
+
+	return srv.ListenAndServe()
+}
+
+func (s *Server) handleMessages(wr http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		log.Infoln("GET /message")
+
+		wr.WriteHeader(http.StatusOK)
+		wr.Write([]byte("default msg"))
+		return
+	case http.MethodPost, http.MethodPut:
+		log.Infof("%v /message", req.Method)
+
+		buf, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			wr.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		s.msgs <- string(buf)
+
+		wr.WriteHeader(http.StatusAccepted)
+		return
+	default:
+		log.Infof("%v unsupported method", req.Method)
+
+		wr.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	log "github.com/sirupsen/logrus"
+
+	"github.com/juicemia/gophercon-hackday/api"
+)
 
 func main() {
-	fmt.Println("hello, world")
+	log.Info("booting api server...")
+
+	msgs := make(chan string)
+
+	// TODO: Take this out. This is here to fake consuming messages.
+	go func() {
+		for {
+			msg := <-msgs
+
+			log.Infof("got message: %v", msg)
+		}
+	}()
+
+	log.Fatal(api.NewServer(":9090", msgs))
 }
